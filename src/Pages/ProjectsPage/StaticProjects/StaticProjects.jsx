6c88fd93c3ed4eb90bc53961cf917782c5bfccee
Marawan Saqr <loew.krs@yahoo.com>
2025-02-23 21:29:41 +0200

    Added Static Porjects Page

--- src/Pages/ProjectsPage/StaticProjects/StaticProjects.jsx
+++ src/Pages/ProjectsPage/StaticProjects/StaticProjects.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
 import { static_projects } from '../../../data.js';
 import { Container, Row, Col } from 'react-bootstrap';
+import './StaticProjects.css';
+
 
 const StaticProjects = () => {
   const [projects] = useState(static_projects);
 
   return (
-    <div className="static-projects-card">
+    <div className="static-projects-card mt-5 mb-5">
       <Container>
         <h2 className='text-center'>Static Projects</h2>
         <Row>
           {projects.map((project, index) => (
             <Col lg={3} key={index}>
-              <div className="box">
+              <div className="box mb-4">
                 <div className="card">
                   <img src={project.img} className="card-img-top" alt={project.title} />
                   <div className="card-body">
-                    <h5 className="card-title">{project.title}</h5>
-                    <p className="card-text">{project.description}</p>
-                    <a href={project.destination}>Show</a>
+                    <h5 className="card-title text-center">{project.title}</h5>
+                    <p className="card-text text-center">{project.description}</p>
+                    <a className='btn btn-success' target="_blank" href={project.destination}>Visit Website</a>
                   </div>
                 </div>
               </div>
--- src/data.js
+++ src/data.js
@@ -1,8 +1,272 @@
 export const static_projects = [
   {
-    img: "/static-projects/amazon-smile.png",
+    img: "/images/amazon-smile.png",
     title: "Amazon Smile",
     description: "Landing Page For Amazon Smile",
     destination: "/Static_Projects/Amazon-Smile/index.html"
-  }
+  },
+  {
+    img: "/images/animated-form.png",
+    title: "Animated Form",
+    description: "Basic Form With high quality image",
+    destination: "/Static_Projects/Animated-Form/index.html"
+  },
+  {
+    img: "/images/article.png",
+    title: "Article",
+    description: "article page with data of this article",
+    destination: "/Static_Projects/Article/index.html"
+  },
+  {
+    img: "/images/A-world.png",
+    title: "A-world",
+    description: "website that show A-world company trips",
+    destination: "/Static_Projects/A-World/index.html"
+  },
+  {
+    img: "/images/bondi.png",
+    title: "Bondi",
+    description: "beautifull design show bondi technology website services",
+    destination: "/Static_Projects/Bondi/index.html"
+  },
+  {
+    img: "/images/building-material.png",
+    title: "Building Material",
+    description: "civil engineer Website With Yellow Theme",
+    destination: "/Static_Projects/Building-Material/index.html"
+  },
+  {
+    img: "/images/cairo-airways.png",
+    title: "Cairo Airways",
+    description: "cairo airways trips represents available trips",
+    destination: "/Static_Projects/Cairo-AirWays/index.html"
+  },
+  {
+    img: "/images/candy.png",
+    title: "Candy",
+    description: "candy services on website with pink theme",
+    destination: "/Static_Projects/Candy/index.html"
+  },
+  {
+    img: "/images/clothes-store.png",
+    title: "Clothes Store",
+    description: "store repesents clothes with types",
+    destination: "/Static_Projects/Clothes-Store/index.html"
+  },
+  {
+    img: "/images/coding.png",
+    title: "Coding",
+    description: "coding website with courses and rates",
+    destination: "/Static_Projects/Coding/index.html"
+  },
+  {
+    img: "/images/creative-thinking.png",
+    title: "Creative Thinking",
+    description: "software house website services",
+    destination: "/Static_Projects/Creative-Thinking/index.html"
+  },
+  {
+    img: "/images/cristiano-ronaldo.png",
+    title: "Cristiano Ronaldo",
+    description: "cristiano ronaldo profile and skills",
+    destination: "/Static_Projects/Cristiano-Ronaldo/index.html"
+  },
+  {
+    img: "/images/dashboard.png",
+    title: "Dashboard",
+    description: "dasboard for personal projects with beautiful design",
+    destination: "/Static_Projects/Dashboard/index.html"
+  },
+  {
+    img: "/images/design-interface.png",
+    title: "Design Interface",
+    description: "interface for project designs interfaces",
+    destination: "/Static_Projects/Design-Interface/index.html"
+  },
+  {
+    img: "/images/dog-house.png",
+    title: "Dog House",
+    description: "services for dogs and how to care of them",
+    destination: "/Static_Projects/Dog-House/index.html"
+  },
+  {
+    img: "/images/egyptian-communication.png",
+    title: "Egyptian Communication",
+    description: "communicate with people and chat around world",
+    destination: "/Static_Projects/Egyptian-Communication/index.html"
+  },
+  {
+    img: "/images/english-kingdom.png",
+    title: "English Kingdom",
+    description: "english website for courses and learning",
+    destination: "/Static_Projects/English-Web/index.html"
+  },
+  {
+    img: "/images/eng-marawan.png",
+    title: "Eng Marawan",
+    description: "animations for eng: marawan cards",
+    destination: "/Static_Projects/Eng-Marawan/index.html"
+  },
+  {
+    img: "/images/eslamy.png",
+    title: "Eslamy",
+    description: "Eslamy websites with islamic categories",
+    destination: "/Static_Projects/Eslamy/index.html"
+  },
+  {
+    img: "/images/fifa20-system.png",
+    title: "FIFA20 System",
+    description: "simulate fifa20 system with smooth transitions",
+    destination: "/Static_Projects/FIFA20-System/index.html"
+  },
+  {
+    img: "/images/front-end-devlopment.png",
+    title: "Front-End Development",
+    description: "front-end website that represents career languages",
+    destination: "/Static_Projects/Front-end-development/index.html"
+  },
+  {
+    img: "/images/fusion-travel.png",
+    title: "Fusion Travel",
+    description: "Fusion Travels with places around world",
+    destination: "/Static_Projects/Fusion-Travel/index.html"
+  },
+  {
+    img: "/images/gamer.png",
+    title: "Gamer",
+    description: "games website that represent games and buy from it",
+    destination: "/Static_Projects/Gamer/index.html"
+  },
+  {
+    img: "/images/gamery.png",
+    title: "Gamery",
+    description: "games website that represent games and buy from it",
+    destination: "/Static_Projects/Gamery/index.html"
+  },
+  {
+    img: "/images/godaddy.png",
+    title: "Go Daddy",
+    description: "simulate go daddy website with business design",
+    destination: "/Static_Projects/Godaddy/index.html"
+  },
+  {
+    img: "/images/great-house.png",
+    title: "Great House",
+    description: "Engineering Projects websites portfolio",
+    destination: "/Static_Projects/Great-House/index.html"
+  },
+  {
+    img: "/images/huddle.png",
+    title: "Huddle",
+    description: "website for huddle portfolio with beautifull design",
+    destination: "/Static_Projects/Huddle/index.html"
+  },
+  {
+    img: "/images/kids-world.png",
+    title: "Kids World",
+    description: "beautiful world for kids with colorize designs",
+    destination: "/Static_Projects/Kids-World/index.html"
+  },
+  {
+    img: "/images/king-of-food.png",
+    title: "King Of Food",
+    description: "food website to order meals and show discounts",
+    destination: "/Static_Projects/King-Of-Food/index.html"
+  },
+  {
+    img: "/images/marawan-animated-resume.png",
+    title: "My Animated Resume",
+    description: "animated resume of me with animated design",
+    destination: "/Static_Projects/Marawan-Animated-Resume/index.html"
+  },
+  {
+    img: "/images/music-colors.png",
+    title: "Music Colors",
+    description: "music team portfolio with time of their parties",
+    destination: "/Static_Projects/Music-Colors/index.html"
+  },
+  {
+    img: "/images/my-portfolio2024.png",
+    title: "My Portfolio 2024",
+    description: "My Portfolio 2024 With Projects",
+    destination: "/Static_Projects/My-Portfolio-2024/index.html"
+  },
+  {
+    img: "/images/news-homepage.png",
+    title: "News Homepage",
+    description: "news homepage With News and sections",
+    destination: "/Static_Projects/news-homepage/index.html"
+  },
+  {
+    img: "/images/open-cams.png",
+    title: "Open Cams",
+    description: "flowers Website With ability to buy flowers",
+    destination: "/Static_Projects/OpenCams/index.html"
+  },
+  {
+    img: "/images/perfume-card.png",
+    title: "Perfume Card",
+    description: "Perfume Card Design With Beautiful Style",
+    destination: "/Static_Projects/Perfume-Card/index.html"
+  },
+  {
+    img: "/images/planes.png",
+    title: "Planes",
+    description: "simulate website to book travels",
+    destination: "/Static_Projects/Planes/index.html"
+  },
+  {
+    img: "/images/porche.png",
+    title: "Porche Store",
+    description: "porche store of porche cars with black design",
+    destination: "/Static_Projects/Porche-Store/index.html"
+  },
+  {
+    img: "/images/simple-form.png",
+    title: "Simple Form",
+    description: "simple form when login route to thanks page",
+    destination: "/Static_Projects/simple-form/index.html"
+  },
+  {
+    img: "/images/student-form.png",
+    title: "Student Form",
+    description: "simple form when login route to another page",
+    destination: "/Static_Projects/Student-Form/index.html"
+  },
+  {
+    img: "/images/the-brand.png",
+    title: "The Brand",
+    description: "the brand Portfolio with it's categories",
+    destination: "/Static_Projects/The-Brand/index.html"
+  },
+  {
+    img: "/images/travel-love.png",
+    title: "Travel Love",
+    description: "travels website with book to each travel",
+    destination: "/Static_Projects/Travel-love/index.html"
+  },
+  {
+    img: "/images/ubisoft-store.png",
+    title: "Ubisoft Store",
+    description: "simulate ubisoft store website with beautifull design",
+    destination: "/Static_Projects/Ubisoft-Store/index.html"
+  },
+  {
+    img: "/images/video-graph.png",
+    title: "Video Graph",
+    description: "video grpaher portfolio with beautiful design",
+    destination: "/Static_Projects/VideoGraph/index.html"
+  },
+  {
+    img: "/images/we-design-things.png",
+    title: "We Design Things",
+    description: "designer team with beautiful portfolio",
+    destination: "/Static_Projects/We-Design-Things/index.html"
+  },
+  {
+    img: "/images/widget-login.png",
+    title: "Widget Login",
+    description: "widget login and signup in one form",
+    destination: "/Static_Projects/Widget-Login/index.html"
+  },
 ];
